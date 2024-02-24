f79fb330dbfbf694768c4699ba9ca075d91f7188
dwi angga <mac@macs-MacBook-Air.local>
2024-02-24 17:03:00 +0800

    mengupload project simple web portfolio

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+const togel = document.querySelector("nav .navbar-ekstra input");
+const navigasi = document.querySelector("nav ul");
+
+togel.addEventListener("click", function(){
+    navigasi.classList.toggle("active");
+});
+
+// alert("Welcome");
